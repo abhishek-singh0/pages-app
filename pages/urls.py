57a91b7320c5b32b29a96ebaad26c9d60a340d58
abhishek-singh0 <abhishek.programs@gmail.com>
2021-08-10 16:00:35 +0530

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from os import name
+from django.urls import path
+from . views import HomePageView,AboutPage
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPage.as_view(), name='about')
+]
